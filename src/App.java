2c56d4b74ec5db76574ea76646974c175be45d92
Lucas Sá <lucas.sodresa@gmail.com>
2021-09-11 14:11:28 +0100

    main program created

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import calculator.Adder;
+import calculator.Calculator;
+import calculator.Divider;
+import calculator.Multiplier;
+import calculator.Subtracter;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        try {
+            Calculator calculadora = new Calculator();
+            Adder adicao = new Adder();
+            Subtracter subtracao = new Subtracter();
+            Multiplier multiplicacao = new Multiplier();
+            Divider divisao = new Divider();
+
+            calculadora.addNumberToHistory(5);
+            calculadora.addNumberToHistory(6);
+
+            calculadora.setOperator(adicao);
+            calculadora.calculate();
+
+            System.out.println(calculadora.getNumbersHistory());
+        } catch (Exception error) {
+            System.out.println(error);
+
+        }
+
     }
 }
