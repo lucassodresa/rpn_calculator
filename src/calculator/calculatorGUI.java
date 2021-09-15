c9d2881b3584552700d8d6e99ea4a65a450db001
Brenno <fonsecabrenno@gmail.com>
2021-09-15 20:16:24 -0300

    gui implementation

---
+++ src/calculator/calculatorGUI.java
@@ -0,0 +1,190 @@
+package calculator;
+import javax.swing.*;
+import java.awt.*;
+
+class calculatorGUI extends JFrame  {
+    // criação de frame
+    static JFrame f;
+ 
+    // criação de textfield e panel
+    static JTextField pilha, campo;
+    static JPanel p;
+ 
+    public static void main(String args[])
+    {
+    
+        f = new JFrame("Calculadora RPN");
+    
+        JPanel p = new JPanel();
+        p.setLayout(null);
+        p.setBackground(Color.gray);
+
+        pilha = new JTextField();
+        pilha.setBorder(BorderFactory.createTitledBorder("Números na memória"));
+        pilha.setEditable(false);
+        pilha.setLayout(null);
+        pilha.setBounds(16,10,350,40);
+        pilha.setFont(new Font("Arial", Font.BOLD, 18));
+        
+        p.add(pilha);
+
+        campo = new JTextField(30);
+        campo.setEditable(false);
+        campo.setLayout(null);
+        campo.setBounds(16,60,145,40);
+        campo.setFont(new Font("Arial", Font.BOLD, 18));
+        campo.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(campo);
+ 
+        // criando botoes
+        JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, bsoma, bsubt, bdivi, bmult, bC, bDel, bEnter, bIgual, bPonto;
+ 
+        // propriedades dos botões
+        b0 = new JButton("0");
+        b0.setLayout(null);
+        b0.setBounds(96,360,60,60);
+        b0.setFont(new Font("Arial", Font.BOLD, 20));
+        b0.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b0);
+
+        b1 = new JButton("1");
+        b1.setLayout(null);
+        b1.setBounds(16,280,60,60);
+        b1.setFont(new Font("Arial", Font.BOLD, 20));
+        b1.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b1);
+
+        b2 = new JButton("2");
+        b2.setLayout(null);
+        b2.setBounds(96,280,60,60);
+        b2.setFont(new Font("Arial", Font.BOLD, 20));
+        b2.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b2);
+
+        b3 = new JButton("3");
+        b3.setLayout(null);
+        b3.setBounds(176,280,60,60);
+        b3.setFont(new Font("Arial", Font.BOLD, 20));
+        b3.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b3);
+
+        b4 = new JButton("4");
+        b4.setLayout(null);
+        b4.setBounds(16,200,60,60);
+        b4.setFont(new Font("Arial", Font.BOLD, 20));
+        b4.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b4);
+
+
+        b5 = new JButton("5");
+        b5.setLayout(null);
+        b5.setBounds(96,200,60,60);
+        b5.setFont(new Font("Arial", Font.BOLD, 20));
+        b5.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b5);
+
+        b6 = new JButton("6");
+        b6.setLayout(null);
+        b6.setBounds(176,200,60,60);
+        b6.setFont(new Font("Arial", Font.BOLD, 20));
+        b6.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b6);
+
+        b7 = new JButton("7");
+        b7.setLayout(null);
+        b7.setBounds(16,120,60,60);
+        b7.setFont(new Font("Arial", Font.BOLD, 20));
+        b7.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b7);
+
+        b8 = new JButton("8");
+        b8.setLayout(null);
+        b8.setBounds(96,120,60,60);
+        b8.setFont(new Font("Arial", Font.BOLD, 20));
+        b8.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b8);
+
+        b9 = new JButton("9");
+        b9.setLayout(null);
+        b9.setBounds(176, 120, 60, 60);
+        b9.setFont(new Font("Arial", Font.BOLD, 20));
+        b9.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(b9);
+ 
+        // criando operadores 
+        bsoma = new JButton("+");
+        bsoma.setLayout(null);
+        bsoma.setBounds(280 , 120, 60, 60);
+        bsoma.setFont(new Font("Arial", Font.BOLD, 20));
+        bsoma.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(bsoma);
+
+        bsubt = new JButton("-");
+        bsubt.setLayout(null);
+        bsubt.setBounds(280,200,60,60);
+        bsubt.setFont(new Font("Arial", Font.BOLD, 20));
+        bsubt.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(bsubt);
+
+        bdivi = new JButton("/");
+        bdivi.setLayout(null);
+        bdivi.setBounds(280,280,60,60);
+        bdivi.setFont(new Font("Arial", Font.BOLD, 20));
+        bdivi.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(bdivi);
+
+        bmult = new JButton("*");
+        bmult.setLayout(null);
+        bmult.setBounds(280,360,60,60);
+        bmult.setFont(new Font("Arial", Font.BOLD, 20));
+        bmult.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(bmult);
+
+        bEnter = new JButton("ENTER");
+        bEnter.setLayout(null);
+        bEnter.setBounds(165,60,75,39);
+        bEnter.setBackground(Color.red);
+        bEnter.setForeground(Color.black);
+        bEnter.setFont(new Font("Arial", Font.BOLD, 12));
+        bEnter.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(bEnter);
+
+        bC = new JButton ("C");
+        bC.setLayout(null);
+        bC.setBounds(320,60,45,39);
+        bC.setFont(new Font("Arial", Font.BOLD, 15));
+        bC.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(bC);
+
+        bDel = new JButton ("Delete");
+        bDel.setLayout(null);
+        bDel.setBounds(245,60,70,39);
+        bDel.setFont(new Font("Arial", Font.BOLD, 12));
+        bDel.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(bDel);
+
+        bIgual = new JButton ("=");
+        bIgual.setLayout(null);
+        bIgual.setBounds(176,360,60,60);
+        bIgual.setFont(new Font("Arial", Font.BOLD, 20));
+        bIgual.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(bIgual);
+
+        bPonto = new JButton (".");
+        bPonto.setLayout(null);
+        bPonto.setBounds(16,360,60,60);
+        bPonto.setFont(new Font("Arial", Font.BOLD, 20));
+        bPonto.setBorder(BorderFactory.createLineBorder(Color.black));
+        p.add(bPonto);
+
+        // configurando jframe
+
+        f.add(p);
+        f.setResizable(false);
+        f.setSize(400, 470);
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    
+}
---
+++ src/calculator/popupERRO.java
@@ -0,0 +1,14 @@
+package calculator;
+import javax.swing.*;
+
+
+public class popupERRO {
+    
+    public static void main(String arg[]) {
+        JOptionPane.showMessageDialog(null,"INSERIR ERRO AQUI!","ERRO", JOptionPane.ERROR_MESSAGE);
+        System.exit(0); 
+        
+    } 
+    
+} 
+
